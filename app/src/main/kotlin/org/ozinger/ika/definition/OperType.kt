cd50d195a0467e23299f2d08c6cdd91da3ffc199
Bae Junehyeon <devunt@gmail.com>
2021-02-12 02:38:51 +0900

    enum 이름 대문자로 통일함

--- app/src/main/kotlin/org/ozinger/ika/definition/OperType.kt
+++ app/src/main/kotlin/org/ozinger/ika/definition/OperType.kt
@@ -3,12 +3,12 @@ package org.ozinger.ika.definition
 import kotlinx.serialization.Serializable
 import org.ozinger.ika.serialization.serializer.EnumValueSerializer
 
-object OperTypeSerializer : EnumValueSerializer<OperType>(OperType::class, OperType.Unknown)
+object OperTypeSerializer : EnumValueSerializer<OperType>(OperType::class, OperType.UNKNOWN)
 
 @Serializable(with = OperTypeSerializer::class)
 enum class OperType(override val value: String) : ValuedEnum {
     NETADMIN("NetAdmin"),
     SERVICE("Services"),
     BOT("Bot"),
-    Unknown(""),
+    UNKNOWN(""),
 }
